8c6616fbe07dc3bd00e994275c9f08bd67756e48
Satomin2864 <satomiyuta0602@gmail.com>
2020-09-05 20:35:15 +0900

    ヘッダーとフッターを作成

---
+++ src/components/footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-footer padless>
+    <v-card class="flex" flat tile>
+      <v-card-actions class="teal darken-2 justify-center">
+        &copy;{{ new Date().getFullYear() }} —
+        <strong>stmn</strong>
+      </v-card-actions>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data: () => ({})
+};
+</script>
---
+++ src/components/header.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app-bar app dense class="px-0">
+    <v-toolbar-items>
+      <v-btn text to="/">
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+    </v-toolbar-items>
+    <v-spacer />
+    <v-toolbar-items>
+      <v-btn text to="/about">
+        <v-icon>mdi-account-circle</v-icon>
+      </v-btn>
+      <v-btn text to="/skills">
+        <v-icon>mdi-dns</v-icon>
+      </v-btn>
+      <v-btn text to="/links">
+        <v-icon>mdi-link-variant</v-icon>
+      </v-btn>
+    </v-toolbar-items>
+    <div class="px-3">
+      <v-switch dense color="gray" v-model="$vuetify.theme.dark" hide-details>
+        <template v-slot:label>
+          <v-icon class="pl-3">{{ $vuetify.theme.dark ? 'fas fa-moon' : 'fas fa-sun' }}</v-icon>
+        </template>
+      </v-switch>
+    </div>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  data: vm => ({
+    initialDark: vm.$vuetify ? vm.$vuetify.theme.dark : false
+  }),
+
+  beforeDestroy() {
+    if (!this.$vuetify) return;
+    this.$vuetify.theme.dark = this.initialDark;
+  }
+};
+</script>
