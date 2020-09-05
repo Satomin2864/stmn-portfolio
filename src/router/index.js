f222057d89c09e84486605d26af158bf6d026438
Satomin2864 <satomiyuta0602@gmail.com>
2020-09-05 20:42:38 +0900

    上げ忘れていたファイルのアップロードとrouter設定更新

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
+const routes = [
   {
     path: '/about',
     name: 'About',
@@ -17,6 +11,14 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/',
+    name: 'Home',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "home" */ '@/components/home.vue')
   }
 ]
 
