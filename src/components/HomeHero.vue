ce61b6e7642598ffc99667c1c595a321b461968a
Satomin2864 <satomiyuta0602@gmail.com>
2020-09-06 19:58:05 +0900

    トップページのヒーローエリアを作成

---
+++ src/components/HomeHero.vue
@@ -0,0 +1,42 @@
+<template>
+  <!-- <section class="home-hero"> -->
+  <!-- <v-container fluid fill-height class="home-hero__content"> -->
+  <v-container fluid fill-height>
+    <v-row>
+      <v-img src="../assets/top.png">
+        <v-overlay :absolute="true" :value="true">
+          <v-col class="home-hero__text">
+            <p>Welcom to my website.</p>
+            <p>I'm a enginner.</p>
+          </v-col>
+        </v-overlay>
+      </v-img>
+    </v-row>
+  </v-container>
+  <!-- </section> -->
+</template>
+
+<style  lang="scss" scoped>
+.home-hero__content {
+  width: 100%;
+  height: 50vh;
+  z-index: 1;
+  //   &::before {
+  //     content: " ";
+  //     background-color: rgba(0, 0, 0, 0.5);
+  //     position: absolute;
+  //     top: 0;
+  //     right: 0;
+  //     bottom: 0;
+  //     left: 0;
+  //     z-index: 2;
+  //   }
+}
+.home-hero__text {
+  color: rgb(245, 248, 245);
+  text-align: center;
+  font-size: 28px;
+  font-weight: bold;
+  z-index: 3;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "home" */ '@/components/home.vue')
+    component: () => import(/* webpackChunkName: "home" */ '../components/home.vue')
   }
 ]
 
