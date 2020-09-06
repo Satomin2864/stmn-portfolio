e2fe57511df4d6791654ddbaa2590adeb271dfec
Satomin2864 <satomiyuta0602@gmail.com>
2020-09-06 19:52:41 +0900

    フッターの表示項目を変更

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,16 +1,27 @@
 <template>
-  <v-footer padless>
+  <v-footer dark padless>
     <v-card class="flex" flat tile>
-      <v-card-actions class="teal darken-2 justify-center">
-        &copy;{{ new Date().getFullYear() }} —
+      <v-card-title class="teal">
+        <strong class="subheading">Get connected with us on social networks!</strong>
+
+        <v-spacer></v-spacer>
+
+        <v-btn v-for="icon in icons" :key="icon" class="mx-4" dark icon>
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-title>
+
+      <v-card-text class="py-2 white--text text-center">
+        {{ new Date().getFullYear() }} —
         <strong>stmn</strong>
-      </v-card-actions>
+      </v-card-text>
     </v-card>
   </v-footer>
 </template>
-
 <script>
 export default {
-  data: () => ({})
+  data: () => ({
+    icons: ["mdi-facebook", "mdi-twitter", "mdi-github"]
+  })
 };
 </script>
